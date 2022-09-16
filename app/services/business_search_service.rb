67c2267ea91e6a2929cb4f6df294e62969eb042a
Bryan Shears <brshears@gmail.com>
2022-09-15 21:15:42 -0600

    create facades, services, poros file structure

---
+++ app/services/business_search_service.rb
@@ -0,0 +1,8 @@
+class BuisnessSearchService 
+
+    def self.conn
+        Faraday.new("https://api.yelp.com/v3") do |faraday| 
+            
+    end 
+
+end 
---
+++ spec/services/buisness_search_service_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe 'Business Service' do 
+    context "class methods" do 
+    
+    end 
+end 
