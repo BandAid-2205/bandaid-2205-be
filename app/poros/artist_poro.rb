da5a814c34a07a09deb5eaafeec94ae3570122cf
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-16 08:57:18 -0500

    Add ArtistPoro with attributes

--- app/poros/artist_poro.rb
+++ app/poros/artist_poro.rb
@@ -1,8 +1,11 @@
-class Artist
+class ArtistPoro
+  attr_reader :name, :bio, :genres, :image_path
+
   def initialize(data)
     @name = data[:name]
     @bio = data[:bio][:summary]
     @genres = get_genres(data[:tags][:tag])
+    @image_path = data[:image][2][:"#text"]
   end
 
   def get_genres(tags)
--- spec/poros/artist_poro_spec.rb
+++ spec/poros/artist_poro_spec.rb
@@ -1,19 +1,17 @@
 require 'rails_helper' 
 
-RSpec.describe Artist do 
+RSpec.describe 'Artist Poro' do 
   it 'exists and has proper Artist attributes' do 
     response = file_fixture('sample_artist_data.txt').read 
 
     data = JSON.parse(response, symbolize_names: true)
-    binding.pry 
 
-    artist = Artist.new(data)
+    artist = ArtistPoro.new(data)
 
-    expect(artist).to be_an Artist 
+    expect(artist).to be_an ArtistPoro
     expect(artist.name).to eq(data[:name])
     expect(artist.bio).to eq(data[:bio][:summary])
     tags = data[:tags][:tag].map { |tag| tag[:name] }
-    binding.pry 
     expect(artist.genres).to eq(tags)
     expect(artist.image_path).to eq(data[:image][2][:"#text"])
   end
