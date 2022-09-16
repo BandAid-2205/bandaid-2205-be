1edbaf62cea72da411bd8b8831180ddf4555bfe6
Bryan Shears <brshears@gmail.com>
2022-09-16 14:10:44 -0600

    implment vcr to services refactor

---
+++ app/services/venue_service.rb
@@ -0,0 +1,12 @@
+class VenueService 
+    def self.conn
+        Faraday.new("https://api.yelp.com/v3/") do |faraday| 
+            faraday.headers['Authorization'] = ENV['yelp_api_key']
+        end 
+    end 
+
+    def self.search_venue(music_venue, location)
+        response = conn.get("businesses/search?term=#{music_venue}&location=#{location}")
+        json = JSON.parse(response.body, symbolize_names: true) if response.status == 200 
+    end 
+end 
