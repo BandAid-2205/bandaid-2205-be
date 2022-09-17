ddb6e90985209333a3054e81d1b0b3259956f569
Bryan Shears <brshears@gmail.com>
2022-09-16 20:34:41 -0600

    refactor venue_poro

--- app/poros/venue_poro.rb
+++ app/poros/venue_poro.rb
@@ -1,10 +1,15 @@
 class VenuePoro
-    attr_reader :name, :rating, :price, :location
+    attr_reader :name, :rating, :price, :location, :id
+
     def initialize(data)
         @name = data[:name]
         @rating = data[:rating]
-        @price = data[:price]
-        @location = data[:location]
+        @price = data[:price] 
+        @location = address(data[:location])
+        @id = data[:id]
     end 
 
+    def address(location)
+        "#{location[:address1]} #{location[:address2]} #{location[:city]}, #{location[:state]} #{location[:zip_code]}"
+    end
 end 
