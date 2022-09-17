09f8751bff88fe2aabcb596d8af8061c1c2aff34
Anna Marie Sterling <sterling.am@gmail.com>
2022-09-17 15:06:01 -0600

    Change venue faker data

--- spec/factories/venues.rb
+++ spec/factories/venues.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     location { Faker::Address.full_address }
     phone { Faker::PhoneNumber.cell_phone }
     price { ['$', '$$', '$$$'].sample }
-    category { Faker::Company.industry }
+    category { Faker::Types.rb_array(len: 2) }
     rating { Faker::Number.within(range: 1..5) }
     user_id { Faker::Number.within(range: 50..75) }
   end
