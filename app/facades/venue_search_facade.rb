48c37cccd366869dbf1b04669167858c2bfc01b7
Bryan Shears <brshears@gmail.com>
2022-09-17 14:42:16 -0600

    add venue search facade

--- app/facades/venue_search_facade.rb
+++ app/facades/venue_search_facade.rb
@@ -1,7 +1,9 @@
 class VenueSearchFacade 
     def self.venue_search(venue, location) 
         venue_json = VenueService.search_venue(venue, location)
-     
+        
+        venue_json[:businesses]
+        
         venue_json[:businesses].map do |business|
             VenuePoro.new(business)
         end  
