dd379069da5176893198d3ded50cb743fc38acfb
Anna Marie Sterling <sterling.am@gmail.com>
2022-09-17 14:46:16 -0600

    Update artist.rb
    Add allow ni to bio

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,7 @@
 class Artist < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :location
-  validates_presence_of :bio
+  validates_presence_of :bio, allow_nil: true
   validates_presence_of :genres
   validates_presence_of :image_path, allow_nil: true
   validates_presence_of :user_id
