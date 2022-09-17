a95a69e7100c35ff773f64dacdb7f0d91f3e7851
Bryan Shears <brshears@gmail.com>
2022-09-17 14:46:35 -0600

    refactor venue_serach_facade and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/artists/search', to: 'artists/search#show'
-      get '/venues/search', to: 'venues/search#show'
+      
       namespace :lastfm do
         get '/search', to: 'artist_info#find_artist'
       end
