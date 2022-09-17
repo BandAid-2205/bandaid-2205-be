d096d82201aa4522ecb07f4ce4690bdc46df736f
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-17 16:13:05 -0500

    Edit factory to account for 4228742287

--- spec/factories/venues.rb
+++ spec/factories/venues.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     name { Faker::Company.name }
     location { Faker::Address.full_address }
     phone { Faker::PhoneNumber.cell_phone }
-    price { ['$', '$$', '$$$'].sample }
+    price { ['$', '$$', '$$$', '$$$$'].sample }
     category { Faker::Company.industry }
     rating { Faker::Number.within(range: 1..5) }
     user_id { Faker::Number.within(range: 50..75) }
