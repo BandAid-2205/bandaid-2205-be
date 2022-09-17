9ccdfa0b28eff3426fec506c5399dac791d70327
Bryan Shears <brshears@gmail.com>
2022-09-17 14:42:54 -0600

    add routes for venue_info_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/artists/search', to: 'artists/search#show'
+      get '/venues/search', to: 'venues/search#show'
       namespace :lastfm do
         get '/search', to: 'artist_info#find_artist'
       end
+      namespace :yelp do 
+        get '/search', to: 'venue_info#find_venue'
+      end 
     end
   end
 end
