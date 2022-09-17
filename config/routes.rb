63fc63f3f41d9642bcf522969295b2a79e3a3f84
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-17 11:42:13 -0500

    Add feature to create Venue details thru POST request from FE

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/artists/search', to: 'artists/search#show'
+      
+      resources :venues, only: [:create]
+
       namespace :lastfm do
         get '/search', to: 'artist_info#find_artist'
       end
