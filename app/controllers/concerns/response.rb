15846d9276628d21f3cf56ae8dbd4a66a5b61afe
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-18 09:05:22 -0500

    Remove unneccessary conditional in Response#venue_json_response

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,10 +1,6 @@
 module Response 
   def venue_json_response(object, status = :ok)
-    if object == nil 
-      render json: VenueSerializer.new(object), status: status 
-    else 
-      render json: VenueSerializer.new(object), status: status 
-    end
+    render json: VenueSerializer.new(object), status: status 
   end
 
   def error_response(message, status)
