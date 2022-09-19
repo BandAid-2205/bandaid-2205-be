53b8bde2f25c8e9e5097ccbc7b6eb9ec6aa89c46
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-19 16:16:41 -0500

    Add happy path test for VenueArtist create

---
+++ spec/requests/api/v1/venue_artists_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper' 
+
+RSpec.describe VenueArtist do 
+  it 'creates a new VenueArtist tied to a Venue and an Artist' do 
+    venue_1 = create(:venue)
+    artist_1 = create(:artist)
+
+    va_params = ({ 
+      venue_id: venue_1.id, 
+      artist_id: artist_1.id, 
+    })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "api/v1/venue_artists", headers: headers, params: JSON.generate(venue_artists: va_params)
+
+    created_venue_artist = VenueArtist.last 
+
+    expect(response).to be_successful
+    expect(created_venue_artist.venue_id).to eq va_params[:venue_id]
+    expect(created_venue_artist.artist_id).to eq va_params[:artist_id]
+    expect(created_venue_artist.booking_status).to eq 0
+  end
+end
