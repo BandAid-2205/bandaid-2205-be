733627cbb9f836c2bf2a038a4c6982fef04d84e1
Anna Marie Sterling <sterling.am@gmail.com>
2022-09-19 16:49:16 -0600

    Add phone number format validation

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,6 +2,8 @@ class Venue < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :location
   validates_presence_of :phone
+  validates_format_of :phone, :with =>  /\d[0-9]\)*\z/ #US only
+  # validates :phone, format: { with: /\A\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}\z/ }
   validates :price, inclusion: ['$', '$$', '$$$', '$$$$']
   validates_presence_of :category, allow_nil: true
   validates_presence_of :rating, allow_nil: true
