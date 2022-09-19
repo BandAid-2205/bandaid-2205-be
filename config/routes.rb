0e32c6e05908babead144ba8cd69f31f035ec729
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-19 17:38:57 -0500

    Update routes for VenueArtist

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  post "api/v1/venues/:id/venue_artists/:artist_id", to: 'venue_artists#create'
+
+  patch "api/v1/venues/:id/venue_artists/:artist_id", to: 'venue_artists#update'
+  
   namespace :api do
     namespace :v1 do
-      resources :venues, only: [:create, :show, :update, :destroy]
+      resources :venues, only: [:create, :show, :update, :destroy] 
+
       resources :artists, only: [:show, :create, :update, :destroy]
-      resources :venue_artists, only: [:create]
+      # resources :venue_artists, only: [:create, :update]
 
       namespace :lastfm do
         get '/search', to: 'artist_info#find_artist'
