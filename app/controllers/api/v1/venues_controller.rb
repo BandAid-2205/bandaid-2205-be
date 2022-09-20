7a37f08245155791a96d55f6373a63f28c247d59
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-19 21:10:00 -0500

    Add call back to set Venue object in VenuesController

--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::VenuesController < ApplicationController
+  before_action :set_venue, only: [:show, :update, :destroy] #callback action 
+
   def index
     render json: VenueSerializer.new(Venue.all)
   end
@@ -9,23 +11,27 @@ class Api::V1::VenuesController < ApplicationController
   end
 
   def show
-    venue = Venue.find_by!(user_id: params[:id])
-    venue_json_response(venue)
+    # venue = Venue.find_by!(user_id: params[:id])
+    venue_json_response(@venue)
   end
 
   def update
-    venue = Venue.find_by!(user_id: params[:id])
-    venue.update_attributes!(venue_params)
-    venue_json_response(venue)
+    # venue = Venue.find_by!(user_id: params[:id])
+    @venue.update_attributes!(venue_params)
+    venue_json_response(@venue)
   end
 
   def destroy
-    venue = Venue.find_by!(user_id: params[:id])
-    render json: Venue.destroy(venue.id)
+    # venue = Venue.find_by!(user_id: params[:id])
+    render json: Venue.destroy(@venue.id)
     head :no_content
   end
 
   private
+    def set_venue 
+      @venue = Venue.find_by!(user_id: params[:id])
+    end
+
     def venue_params
       params.require(:venue).permit(:name, :location, :phone, :price, :rating, :category, :user_id)
     end
