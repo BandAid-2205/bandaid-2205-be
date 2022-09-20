09661e62a757da4f45ebbf987f2caf8271b0ba77
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-19 22:50:43 -0500

    add callback to set @venue and @artist

--- app/controllers/venue_artists_controller.rb
+++ app/controllers/venue_artists_controller.rb
@@ -1,8 +1,8 @@
-class VenueArtistsController < ApplicationController 
+class VenueArtistsController < ApplicationController
+  before_action :set_venue_and_artist, only: [:create] #callback action 
+
   def create 
-    venue = Venue.find_by!(user_id: va_params[:venue_id])
-    artist = Artist.find_by!(user_id: va_params[:artist_id])
-    va = VenueArtist.create!(venue_id: venue.id, artist_id: artist.id)
+    va = VenueArtist.create!(venue_id: @venue.id, artist_id: @artist.id)
     venue_artist_json_response(va) 
   end
 
@@ -20,5 +20,10 @@ class VenueArtistsController < ApplicationController
   private 
     def va_params
       params.require(:venue_artist).permit(:venue_id, :artist_id, :booking_status)
+    end
+
+    def set_venue_and_artist
+      @venue = Venue.find_by!(user_id: va_params[:venue_id])
+      @artist = Artist.find_by!(user_id: va_params[:artist_id])
     end
 end
