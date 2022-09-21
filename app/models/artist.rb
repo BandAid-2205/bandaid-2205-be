ed72f6bbed14f4f8769a4812e2f75c4fcbb327bb
Anna Marie Sterling <sterling.am@gmail.com>
2022-09-20 20:46:21 -0600

    feat/Add attribute to Artist :bookings

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,8 +5,12 @@ class Artist < ApplicationRecord
   validates_presence_of :genre
   validates_presence_of :image_path, allow_nil: true
   validates :image_path, allow_blank: true, format: { with: %r{.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG or PNG image.' }
-  validates_presence_of :user_id
+  validates_presence_of :user_id, unique: true
 
   has_many :venue_artists, dependent: :destroy
   has_many :venues, through: :venue_artists
+
+  def bookings
+    self.venues.references(:venue_artists).pluck(:name, :booking_status).map { |name, booking_status| {venue_name: name, booking_status: booking_status}}
+  end
 end
--- app/models/venue_artist.rb
+++ app/models/venue_artist.rb
@@ -1,10 +1,10 @@
 class VenueArtist < ApplicationRecord
   validates_presence_of :venue_id
   validates_presence_of :artist_id
-  validates_presence_of :booking_status
+  validates_presence_of :booking_status, inclusion: [0, 1, 2]
 
   belongs_to :venue
   belongs_to :artist
 
-  enum booking_status: { 'pending' => 0, 'accepted' => 1, 'rejected' => 2 }
+  enum booking_status: [:pending, :accepted, :rejected]
 end
